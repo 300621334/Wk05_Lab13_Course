c66271a59481ea5fdd81ae313a52734aa7eec79e
300621334 <shafiqu@my.centennialcollege.ca>
2016-09-19 17:49:16 -0400

    initial commit

---
+++ CourseDemo/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseDemo
+{
+    enum DisplayOption { All, Code, Name, Prerequsite, Semester }
+
+    class Course
+    {
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+        public string Prerequsites { get; private set; }
+        public int Semester { get; private set; }
+        public string Description { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("Course Code: {0}\nCourse Name: {1}\nSemester: {2}\nRequirements: {3}\nDescription: {4}",Code, Name, Semester, Prerequsites, Prerequsites.Substring(0, 60));
+        }
+        public Course(string code, string name, string description, string semester, string prerequsites)
+        { Code = code;Description = description;Name = name;Prerequsites = prerequsites;Semester = Convert.ToInt32(semester); }
+    }
+}
---
+++ CourseDemo/CourseManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CourseDemo
+{
+
+    static class CourseManager
+    {
+
+        public static List<Course> courses;
+        public static void Display(DisplayOption option, string toMatch = "")
+        {
+            foreach(Course x in courses)
+            {
+                switch(option)
+                { case DisplayOption.All:
+                        Console.WriteLine(x);
+                        break;
+                    case DisplayOption.Code:
+                        if (toMatch == x.Code)
+                            { Console.WriteLine(x); }
+                            break;
+                    case DisplayOption.Name:
+                        if (toMatch == x.Name)
+                        { Console.WriteLine(x); }
+                        break;
+                    case DisplayOption.Semester:
+                        if (toMatch == x.Semester.ToString())
+                        { Console.WriteLine(x); }
+                        break;
+                    case DisplayOption.Prerequsite:
+                        if (x.Prerequsites.Contains(toMatch))
+                        { Console.WriteLine(x); }
+                        break;
+                }
+            }
+        }
+        public static void LoadCourses(string fileName)
+        {
+            courses = new List<Course>();
+            using (TextReader reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    if (line==null) { break; }
+                    string[] splitLine = line.Split('~');
+                    courses.Add(new Course(splitLine[0], splitLine[1], splitLine[2], splitLine[3], splitLine[4]));
+                }
+            }
+        }
+
+    }
+}
